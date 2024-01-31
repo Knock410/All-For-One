5177644b5ce3a06b3c6695fa9e157e4d693cbadc
Knock <enoch10evelyn@gmail.com>
2024-01-31 11:07:24 -0500

    Javascript is linked to HTML and  intitial function has been created

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+window.alert("You're good and going to be greaterfd.");
+
+function fight() {
+  window.alert("The fight has begun!");
+  playerName = window.prompt("What is your name fighter");
+  window.alert(
+    "This is a league where  everyone is for only one. Themselves. Through combating various opponents at once the winner will win all. " +
+      playerName +
+      " it's time to engage and rise to the top!"
+  );
+}
+
+fight();
