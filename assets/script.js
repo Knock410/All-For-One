c2054cc29e4df1977ac70da32d1a451b38941a59
Knock <enoch10evelyn@gmail.com>
2024-01-31 12:21:14 -0500

    Variables, prompts and function edited overall

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,22 @@
-window.alert("You're good and going to be greaterfd.");
+window.alert("You're good and going to be greater.");
+var playerName = window.prompt("What is your name fighter");
+var playerHealth = 100;
+var playerAttack = 10;
+
+var enemyName = "Draco";
+var enemyHealth = 30;
+var enemyAttack = 20;
 
 function fight() {
   window.alert("The fight has begun!");
-  playerName = window.prompt("What is your name fighter");
+
   window.alert(
     "This is a league where  everyone is for only one. Themselves. Through combating various opponents at once the winner will win all. " +
       playerName +
       " it's time to engage and rise to the top!"
+     
   );
+  console.log(playerName); 
 }
 
-fight();
+ fight();
