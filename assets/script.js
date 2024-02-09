c04a021ebc6364c773341c45a4b45a6f3586f444
Knock <enoch10evelyn@gmail.com>
2024-02-08 20:14:18 -0500

    Control Flow statement has been added the code to check the status of the players health after a combat round

--- assets/script.js
+++ assets/script.js
@@ -5,18 +5,36 @@ var playerAttack = 10;
 
 var enemyName = "Draco";
 var enemyHealth = 30;
-var enemyAttack = 20;
+var enemyAttack = 200;
 
-function fight() {
+var fight = function() {
   window.alert("The fight has begun!");
 
   window.alert(
-    "This is a league where  everyone is for only one. Themselves. Through combating various opponents at once the winner will win all. " +
+    "This is a league where  everyone is for only one. Themselves. Through combating various opponents at once the winner will win it all. " +
       playerName +
-      " it's time to engage and rise to the top!"
+      " it's time to engage and rise to the top!");
+
+      //Subtract the value of the playerattack from the value of the enemyhealth and use that result to update the enemyhealth
+    enemyHealth = enemyHealth - playerAttack
+    
+      //log a message in the console to show it worked
+      console.log(playerName + " attacked " + enemyName + ". " + enemyName + " has " + enemyHealth + " health remaining !");
+      //subtact the value of the enemyattack fromt he valued of the playerhealth and use the result to update the playerhealth 
+      playerHealth = playerHealth- enemyAttack
+      //log a resulting message to show it worked in the console 
+      console.log(enemyName + " attacked " + playerName + ". " + playerName + " has " + playerHealth + " health remaining !")
+
+      //Check the fighters health
+      if (playerHealth <= 0) {
+        console.log("You were knocked out, better luck next time!")
+      }
+      else {console.log("That wasn't enough to take " + playerName + " out the battle !!!")}
+
+    };
+  
      
-  );
-  console.log(playerName); 
-}
+  
+ 
 
  fight();
