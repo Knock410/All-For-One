354f80cfecb06017cc13983de0c420dfd5646146
Knock <enoch10evelyn@gmail.com>
2024-02-11 22:31:16 -0500

    Different routes for the player to choose. Choose to fight or skip. Skipping comes with a cost

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,14 @@ window.alert("You're good and going to be greater.");
 var playerName = window.prompt("What is your name fighter");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 var enemyName = "Draco";
 var enemyHealth = 30;
 var enemyAttack = 20;
 
 var fight = function () {
-  window.alert("The fight has begun!");
+  window.alert("Welcome to Random Encounter!!!!!");
 
   window.alert(
     "This is a league where  everyone is for only one. Themselves. Through combating various opponents at once the winner will win it all. " +
@@ -37,12 +38,14 @@ var fight = function () {
         enemyHealth +
         " health remaining !"
     );
-// Check enemy status 
+    // Check enemy status
     if (enemyHealth <= 0) {
       window.alert(enemyHealth + " has fallen !");
     } else {
-     window.alert(
-        "Even with that devastating attack " + enemyName + " is still standing !"
+      window.alert(
+        "Even with that devastating attack " +
+          enemyName +
+          " is still standing !"
       );
     }
     //Enemy Attack:subtact the value of the enemyattack fromt he valued of the playerhealth and use the result to update the playerhealth
@@ -67,16 +70,33 @@ var fight = function () {
         "That wasn't enough to take " + playerName + " out the battle !!!"
       );
     }
-  }
-  else if (promptFight === "skip" || promptFight === "SKIP") {
-    window.alert(" Not Ready? A withdrawal fee will be charged to you're account. Come back when you're ready to to rumble");
-  }
-  else {
-    window.alert(" Are you fighting or Not ???");
-  }
-  
+  } else if (promptFight === "skip" || promptFight === "SKIP") {
+    var confirmSkip = window.confirm(" Are you sure you want to withdraw? ");
 
-  };
+    if (confirmSkip) {
+      window.alert(
+        " Not Ready? A withdrawal fee will be charged to you're account. Come back when you're ready to to rumble."
+      );
+      var fee = 2;
 
+      playerMoney = playerMoney - fee;
+      window.alert(
+        playerName +
+          " paid a " +
+          fee +
+          " dollar withrawal fee. " +
+          playerName +
+          " has " +
+          playerMoney +
+          " dollars left."
+      );
+    } else {
+      window.alert(" Are you fighting or not ???");
+      fight();
+    }
+  } else {
+    window.alert(" Please choose to proceed into the fray or to back out!!!");
+  }
+};
 
 fight();
